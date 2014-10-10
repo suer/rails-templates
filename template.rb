9fe32941fc0b3be3d298c9935c0f3b90f8c0672d
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-10-10 22:06:16 +0900

    Added: slim

--- template.rb
+++ template.rb
@@ -1,3 +1,4 @@
+# RSpec
 gem_group :development, :test do
   gem 'rspec-rails'
 end
@@ -7,3 +8,16 @@ run 'bundle install --path .bundle'
 generate 'rspec:install'
 remove_dir 'test'
 
+# Slim
+gem 'slim-rails'
+run 'bundle install --path .bundle'
+
+# config
+application <<-GENERATORS
+config.generators.test_framework = :rspec
+    config.generators.template_engine = :slim
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
+GENERATORS
+
+run 'bundle exec spring stop'
