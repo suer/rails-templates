4ed8bf16aa7e10f8fa5a366dc9f3d4f3b7b16b82
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-10-12 07:58:33 +0900

    Added: bootstrap

--- template.rb
+++ template.rb
@@ -35,19 +35,40 @@ devise_for :users, controllers: { omniauth_callbacks: 'authentication' }
     delete 'sign_out', to: 'authentication#logout', as: :destroy_user_session
   end
 ROUTE
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/models/user.rb', './app/models/user.rb'
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/models/settings.rb', './app/models/settings.rb'
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/config/settings.yml', './config/settings.yml'
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/config/initializers/devise.rb', './config/initializers/devise.rb'
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/db/migrate/20140720020818_devise_create_users.rb', './db/migrate/20140720020818_devise_create_users.rb'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/models/user.rb', 'app/models/user.rb'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/models/settings.rb', 'app/models/settings.rb'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/config/settings.yml', 'config/settings.yml'
+
+remove_file 'config/initializers/devise.rb'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/config/initializers/devise.rb', 'config/initializers/devise.rb'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/db/migrate/20140720020818_devise_create_users.rb', 'db/migrate/20140720020818_devise_create_users.rb'
 run 'bundle exec rake db:migrate'
 
 # top page
 remove_file 'app/views/layouts/application.html.erb'
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/views/layouts/application.html.slim', './app/views/layouts/application.html.slim'
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/views/top/index.html.slim', './app/views/top/index.html.slim'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/views/layouts/application.html.slim', 'app/views/layouts/application.html.slim'
 generate :controller, 'top index'
-get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/controllers/top_controller.rb', './app/controllers/top_controller.rb'
+remove_file 'app/views/top/index.html.slim'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/views/top/index.html.slim', 'app/views/top/index.html.slim'
+remove_file 'app/controllers/top_controller.rb'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/controllers/top_controller.rb', 'app/controllers/top_controller.rb'
 route "root to: 'top\#index'"
 
+remove_file 'app/assets/javascripts/application.js'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/assets/javascripts/application.js', 'app/assets/javascripts/application.js'
+
+remove_file 'app/assets/stylesheets/application.css'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/assets/stylesheets/application.css.scss', 'app/assets/stylesheets/application.css.scss'
+get 'https://raw.githubusercontent.com/suer/rails-templates/master/app/assets/stylesheets/main.css.scss', 'app/assets/stylesheets/main.css.scss'
+
+route "root to: 'top\#index'"
+
+
 run 'bundle exec spring stop'
+
+# Bootstrap
+#gem 'sass-rails', '~> 4.0.3'
+gem 'bootstrap-sass', '~> 3.0.3.0'
+run 'bundle install --path .bundle'
+run 'bundle exec rails g bootstrap:install'
+run 'bundle exec rails g bootstrap:layout application fluid'
