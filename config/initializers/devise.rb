7612b47878c297cedfc79b917a225974ce00f4ae
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-10-15 23:43:50 +0900

    Fixed: env is nil

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,12 +2,8 @@ Devise.setup do |config|
   require 'devise/orm/active_record'
   config.secret_key = ENV['DEVISE_SECRET_KEY']
   config.sign_out_via = :delete
-  provider = if ENV['OMNIAUTH_PROVIDER']
-    ENV['OMNIAUTH_PROVIDER'].to_sym
-  else
-    :github
-  end
-  args =  JSON.parse(ENV['OMNIAUTH_ARGS'])
+  provider = ENV['OMNIAUTH_PROVIDER'].nil? ? :github : ENV['OMNIAUTH_PROVIDER'].to_sym
+  args = ENV['OMNIAUTH_ARGS'].nil? ? nil : JSON.parse(ENV['OMNIAUTH_ARGS'])
   case args
   when NilClass
     config.omniauth provider
