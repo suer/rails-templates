8fb56a993491711a51b331a46284b68b449d4155
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-11-06 00:05:41 +0900

    config.serve_static_assets = true

--- template.rb
+++ template.rb
@@ -22,6 +22,8 @@ config.generators.test_framework = :rspec
     config.generators.javascripts = false
 GENERATORS
 
+gsub_file('config/environments/production.rb', /config\.serve_static_assets = false/, 'config.serve_static_assets = true')
+
 # devise
 gem 'devise'
 gem 'omniauth'
